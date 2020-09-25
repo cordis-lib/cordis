19a68030de3224f1f534c12118d6326d70f117be
didinele <didinele.dev@gmail.com>
2020-09-25 19:26:32 +0300

    fix(Handlers): replace old function signature

--- services/gateway/src/handlers/MESSAGE_REACTION_REMOVE.ts
+++ services/gateway/src/handlers/MESSAGE_REACTION_REMOVE.ts
@@ -1,7 +1,7 @@
 import { BaseReactionData, Message } from '@cordis/types';
 import { Handler } from '../Handler';
 
-const messageReactionRemove: Handler<BaseReactionData> = async (data, service, redis, rest, botId) => {
+const messageReactionRemove: Handler<BaseReactionData> = async (data, service, redis, rest, [botUser]) => {
   const rawMessage = await redis.hget(`${data.channel_id}_messages`, data.message_id);
   const message: Message | null = rawMessage
     ? JSON.parse(rawMessage)
@@ -15,7 +15,7 @@ const messageReactionRemove: Handler<BaseReactionData> = async (data, service, r
       .findIndex(r => r.emoji.id === data.emoji.id || r.emoji.name === data.emoji.name);
     if (existingIndex !== -1) {
       const reaction = message.reactions[existingIndex];
-      reaction.me = data.user_id === botId;
+      reaction.me = data.user_id === botUser.id;
       if (--reaction.count <= 0) message.reactions.splice(existingIndex, 1);
       else message.reactions.splice(existingIndex, 0, reaction);
     }
