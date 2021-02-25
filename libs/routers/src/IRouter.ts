47defd00672332b8557ccb17ffd5a42d707a8c55
didinele <didinele.dev@gmail.com>
2021-02-25 08:29:01 +0200

    fix(IRouter): proper types

--- libs/routers/src/IRouter.ts
+++ libs/routers/src/IRouter.ts
@@ -1,9 +1,9 @@
 import type { File, StringRecord } from '@cordis/rest';
 
 export type IRouter = {
-  get<T>(options?: { query?: StringRecord }): T;
-  delete<T>(options?: { data?: StringRecord; reason?: string }): T;
-  patch<T>(options: { data: StringRecord; reason?: string }): T;
-  put<T>(options: { data: StringRecord; reason?: string }): T;
-  post<T>(options: { data: StringRecord; reason?: string; files: File[] }): T;
+  get<T, Q extends StringRecord | string = StringRecord>(options?: { query?: Q }): T;
+  delete<T, D extends StringRecord = StringRecord>(options?: { data?: D; reason?: string }): T;
+  patch<T, D extends StringRecord>(options: { data: D; reason?: string }): T;
+  put<T, D extends StringRecord>(options: { data: D; reason?: string }): T;
+  post<T, D extends StringRecord>(options: { data: D; reason?: string; files?: File[] }): T;
 } & { [key: string]: IRouter };
