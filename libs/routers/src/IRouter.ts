eaefa0b650220def3ab80d0e3b99a83974062607
didinele <didinele.dev@gmail.com>
2021-02-25 20:49:27 +0200

    fix(IRouter): return types

--- libs/routers/src/IRouter.ts
+++ libs/routers/src/IRouter.ts
@@ -1,9 +1,9 @@
 import type { File, StringRecord, RequestBodyData } from '@cordis/rest';
 
 export type IRouter = {
-  get<T, Q extends StringRecord | string = StringRecord>(options?: { query?: Q }): T;
-  delete<T, D extends RequestBodyData = RequestBodyData>(options?: { data?: D; reason?: string }): T;
-  patch<T, D extends RequestBodyData>(options: { data: D; reason?: string }): T;
-  put<T, D extends RequestBodyData>(options: { data: D; reason?: string }): T;
-  post<T, D extends RequestBodyData>(options: { data: D; reason?: string; files?: File[] }): T;
+  get<T, Q extends StringRecord | string = StringRecord>(options?: { query?: Q }): Promise<T>;
+  delete<T, D extends RequestBodyData = RequestBodyData>(options?: { data?: D; reason?: string }): Promise<T>;
+  patch<T, D extends RequestBodyData>(options: { data: D; reason?: string }): Promise<T>;
+  put<T, D extends RequestBodyData>(options: { data: D; reason?: string }): Promise<T>;
+  post<T, D extends RequestBodyData>(options: { data: D; reason?: string; files?: File[] }): Promise<T>;
 } & { [key: string]: IRouter };
