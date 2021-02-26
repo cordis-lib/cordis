75aea1c6a639721aae5f3d78e9abe11435843ecb
Zaid "Nico <me@zaid-arshad.tech>
2021-02-26 18:17:45 -0500

    init makeRestUtils func

---
+++ libs/util/src/index.ts
@@ -0,0 +1,6 @@
+import type { RestManager } from '@cordis/rest';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function makeRestUtils(restRouter: RestManager) {
+  return {};
+}
