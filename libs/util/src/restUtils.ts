2813d12c556cff145a35264f97700c2a461bddcf
Zaid "Nico <me@zaid-arshad.tech>
2021-03-06 21:16:40 -0500

    feat: invite routes

--- libs/util/src/restUtils.ts
+++ libs/util/src/restUtils.ts
@@ -1,11 +1,111 @@
 import * as dAPI from 'discord-api-types/v8';
 import { RestManager } from '@cordis/rest';
 
+/*
+Channel
+ editChannel - encapsulate permissions
+ fetchChannelMessages/fetchChannelMessage
+ sendMessage
+ crosspostMessage
+ addMessageReaction
+ deleteOwnMessageReaction
+ deleteUserReaction
+ deleteAllReactions
+ deleteReaction
+ fetchMessageReactions
+ editMessage
+ deleteMessage
+ bulkDeleteMessages
+ fetchChannelInvites
+ createChannelInvite
+ followNewsChannel
+ startTyping
+ fetchPinnedChannelMessages
+ deletePinnedChannelMessage
+ addPinnedChannelMessage
+ groupDMAddRecipient
+ groupDMRemoveRecipient
+
+Emoji
+ fetchGuildEmojis
+ fetchGuildEmoji
+ createGuildEmoji
+ editGuildEmoji
+ deleteGuildEmoji
+
+Guild
+ createGuild
+ fetchGuild
+ fetchGuildPreview
+ editGuild
+ deleteGuild
+ fetchGuildChannels
+ createGuildChannel
+ fetchGuildMember
+ addGuildMember
+ editGuildMember
+ setNickname
+ addRole
+ removeRole
+ kickMember
+ fetchGuildBans
+ fetchGuildBan
+ banMember
+ unbanMember
+ fetchRoles
+ createRole
+ setRolePosition
+ editRole
+ deleteRole
+ pruneGuild
+ getPredictedPruneResult
+ fetchGuildVoiceRegions
+ fetchGuildInvites
+ createGuildIntegration
+ editGuildIntegration
+ deleteGuildIntegration
+ syncGuildIntegration
+ fetchGuildWidget
+ editGuildWidget (settings too)
+ fetchGuildVanityURL
+ fetchGuildWidgetImage
+
+Invite
+ fetchInvite
+ deleteInvite
+
+Template
+ fetchTemplate
+ createGuildFromTemplate
+ fetchGuildTemplates
+ createGuildTemplate
+ syncGuildTemplate
+ modifyGuildTemplate
+ deleteGuildTemplate
+
+User
+ fetchCurrentUser
+ fetchUser
+ editCurrentUser
+ leaveGuild
+ fetchDMs
+ createDM
+ createGroupDM
+ getUserConnections
+
+
+    fetchChannel: (...args: Parameters<typeof dAPI.Routes.channel>) => rest.get<dAPI.APIChannel>(dAPI.Routes.channel(...args)),
+    deleteChannel: (...args: Parameters<typeof dAPI.Routes.channel>) => rest.delete<dAPI.APIChannel>(dAPI.Routes.channel(...args)),
+*/
+
+
 interface webhookIdOrToken { webhookID: dAPI.Snowflake; webhookToken?: string }
 
 export const makeRestUtils = (rest: RestManager) => (
   {
     fetchAuditLogs: (guildID: dAPI.Snowflake, options?: dAPI.RESTGetAPIAuditLogQuery) => rest.get<dAPI.APIAuditLog, dAPI.RESTGetAPIAuditLogQuery>(dAPI.Routes.guildAuditLog(guildID), { query: options }),
+    fetchInvite: (...args: Parameters<typeof dAPI.Routes.invite>) => rest.get<dAPI.RESTGetAPIInviteResult>(dAPI.Routes.invite(...args)),
+    deleteInvite: (...args: Parameters<typeof dAPI.Routes.invite>) => rest.delete<dAPI.RESTDeleteAPIInviteResult>(dAPI.Routes.invite(...args)),
     createWebhook: (channelID: dAPI.Snowflake, data: dAPI.RESTPostAPIChannelWebhookJSONBody) => rest.post<dAPI.APIWebhook, dAPI.RESTPostAPIChannelWebhookJSONBody>(dAPI.Routes.channelWebhooks(channelID), { data }),
     fetchWebhooks: (...args: Parameters<typeof dAPI.Routes.channelWebhooks>) => rest.get<dAPI.RESTGetAPIChannelWebhooksResult>(dAPI.Routes.channelWebhooks(...args)),
     fetchWebhook: (...args: Parameters<typeof dAPI.Routes.webhook>) => rest.get<dAPI.RESTGetAPIWebhookResult>(dAPI.Routes.webhook(...args)),
