6c3fbbd6bc1e9f4b4c68075786f9a390eb353a9b
didinele <didinele.dev@gmail.com>
2021-03-12 11:44:09 +0200

    feat(gateway): resuming when connecting for the first time

--- libs/gateway/src/websocket/Cluster.ts
+++ libs/gateway/src/websocket/Cluster.ts
@@ -3,7 +3,8 @@ import {
   WebsocketConnection,
   WebsocketConnectionStatus,
   WebsocketConnectionOptions,
-  WebsocketConnectionDestroyOptions
+  WebsocketConnectionDestroyOptions,
+  WebsocketConnectionConnectOptions
 } from './WebsocketConnection';
 import { stripIndent } from 'common-tags';
 import { RestManager, MemoryMutex, RedisMutex } from '@cordis/rest';
@@ -228,8 +229,9 @@ export class Cluster extends EventEmitter {
 
   /**
    * Spawns all of the shards (if not yet spawned) and connects each one to the gateway
+   * @param options Array of connection options for each shard
    */
-  public async connect() {
+  public async connect(options?: (WebsocketConnectionConnectOptions | undefined)[]) {
     if (!this.shards.length) {
       const {
         url,
@@ -254,7 +256,7 @@ export class Cluster extends EventEmitter {
       }
     }
 
-    return Promise.all(this.shards.map(shard => shard.connect()));
+    return Promise.all(this.shards.map((shard, i) => shard.connect(options?.[i])));
   }
 
   /**
