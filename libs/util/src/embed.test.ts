5c6c3500b5157a87073c62475d1034fc7c8092aa
Zaid "Nico <me@zaid-arshad.tech>
2021-03-12 08:28:01 -0500

    tests: improve the embed tests

--- libs/util/src/embed.test.ts
+++ libs/util/src/embed.test.ts
@@ -21,7 +21,7 @@ test('Data value checks', () => {
   const date = new Date('2021-03-08T14:40:27.875Z');
   const color = 16753920;
 
-  const embed2 = buildEmbed({
+  let embed2 = buildEmbed({
     title: 'testing title!',
     description: 'testing description',
     url: 'https://google.com',
@@ -44,4 +44,40 @@ test('Data value checks', () => {
   expect(embed2.timestamp).toBe(date.toString());
   expect(embed2.color).toBe(color);
   expect(embed2.fields).toStrictEqual([]);
+
+  embed2 = embed2
+    .setAuthor('testing_author_3')
+    .setColor(16753920)
+    .setDescription('I am testing changing descriptions')
+    .setFooter('bing bong', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    .setImage('https://static.wikia.nocookie.net/youtubepoop/images/2/2a/Rick_Ashley.png/revision/latest/scale-to-width-down/340?cb=20130102175058')
+    .setThumbnail('https://static.wikia.nocookie.net/youtubepoop/images/2/2a/Rick_Ashley.png/revision/latest/scale-to-width-down/340?cb=20130102175058')
+    .setTitle('Never gonna give you up')
+    .setURL('http://yahoo.com')
+    .addFields({ name: 'never gonna', value: 'let you down', inline: true }, { name: 'never gonna run around and', value: 'desert you' });
+
+  expect(embed2.toJSON()).toMatchObject(
+    {
+
+      title: 'Never gonna give you up',
+      description: 'I am testing changing descriptions',
+      url: 'http://yahoo.com',
+      color: 16753920,
+      fields: [{ name: 'never gonna', value: 'let you down', inline: true }, { name: 'never gonna run around and', value: 'desert you' }],
+      thumbnail: {
+        url: 'https://static.wikia.nocookie.net/youtubepoop/images/2/2a/Rick_Ashley.png/revision/latest/scale-to-width-down/340?cb=20130102175058'
+      },
+      image: {
+        url: 'https://static.wikia.nocookie.net/youtubepoop/images/2/2a/Rick_Ashley.png/revision/latest/scale-to-width-down/340?cb=20130102175058'
+      },
+      author: {
+        name: 'testing_author_3'
+      },
+      type: 'rich',
+      footer: {
+        text: 'bing bong',
+        icon_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+      }
+    }
+  );
 });
