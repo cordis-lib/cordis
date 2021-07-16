27226a81d1c6c41a092ec881487adfb6c91b21d4
didinele <didinele.dev@gmail.com>
2021-07-16 14:48:07 +0000

    fix(GatewayService): properly handle cases where the gateway may stall

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -4,6 +4,11 @@ import { Cluster } from '@cordis/gateway';
 import type { DiscordEvents } from '@cordis/common';
 
 const main = async () => {
+  const timeout = setTimeout(() => {
+    console.error('Timed out from 15 seconds of inactivity');
+    process.exit(1);
+  }, 15000);
+
   const { channel, connection } = await createAmqp(config.amqpHost);
   connection
     .on('error', e => console.error(`[AMQP ERROR]: ${e}`))
@@ -15,13 +20,18 @@ const main = async () => {
   const service = new RoutingServer<keyof DiscordEvents, DiscordEvents>(channel);
   const cluster = new Cluster(config.auth, config.ws);
 
+  const ref = (fn: () => any) => {
+    timeout.refresh();
+    fn();
+  };
+
   cluster
-    .on('disconnecting', id => console.log(`[DISCONNECTING]: Shard id ${id}`))
-    .on('reconnecting', id => console.log(`[RECONNECTING]: Shard id ${id}`))
-    .on('open', id => console.log(`[CONNECTION OPEN]: Shard id ${id}`))
-    .on('error', (err, id) => console.error(`[SHARD ERROR]: Shard id ${id}`, err))
-    .on('ready', () => console.log('[READY]: All shards have fully connected'))
-    .on('dispatch', data => service.publish(data.t, data.d));
+    .on('disconnecting', id => ref(() => console.log(`[DISCONNECTING]: Shard id ${id}`)))
+    .on('reconnecting', id => ref(() => console.log(`[RECONNECTING]: Shard id ${id}`)))
+    .on('open', id => ref(() => console.log(`[CONNECTION OPEN]: Shard id ${id}`)))
+    .on('error', (err, id) => ref(() => console.error(`[SHARD ERROR]: Shard id ${id}`, err)))
+    .on('ready', () => ref(() => console.log('[READY]: All shards have fully connected')))
+    .on('dispatch', data => ref(() => service.publish(data.t, data.d)));
 
   if (config.debug) {
     cluster.on('debug', (info, id) => console.log(`[DEBUG]: Shard id ${id}`, info));
